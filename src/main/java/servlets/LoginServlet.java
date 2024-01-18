f0fe84b42bee4d8f44177f9a252a82ff1a492a2f
Pranay Dhoble <124166406+Pranay-C-Dhoble@users.noreply.github.com>
2024-01-19 01:29:17 +0530

    Servlet & JSP

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(urlPatterns = "/login", name = "LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Inside Login Servlet");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Inside Login Servlet</h1>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Its my first JSP And Servlet program.</h2>
+<p>Today is: <%= (new java.util.Date()).toLocaleString()%></p>
+<p><a href="index.jsp">Back to index</a></p>
+
+<br>
+<a href ="login">Click here to go to the next page</a>
+</body>
+</html>
